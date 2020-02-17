8bfae3e102ed0fb1416b1c7207b89e0d8f8556e0
ahmed eldakhakhny <ahmed0xenon@gmail.com>
2020-02-17 16:22:14 +0200

    accepted solution

--- solution.cpp
+++ solution.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 int main() {
-    cout << "Hello, World! one more time ";
+    int intputsize = 101; 
+    char input[intputsize];
+    int count = 0 ;
+    int lastCharAccii = int('a');
+    cin >> input;
 
+    for (int i = 0; i < intputsize  && input[i] != '\0'; i++)
+    {
+        int currentCharAccii = int(input[i]);
+        int diff =  abs(currentCharAccii-lastCharAccii);
+        if( diff >13 ){
+            diff = 26-diff;
+        }
+        count += diff;
+        lastCharAccii = currentCharAccii;
+    }
+    cout << count;
+    
     return 0;
 }
