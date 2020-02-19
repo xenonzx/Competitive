6745474e077a4201b5d5ac564b1fb74e61397541
ahmed eldakhakhny <ahmed0xenon@gmail.com>
2020-02-19 20:21:31 +0200

    updating template

--- solution.cpp
+++ solution.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 int main(){
-    char str1[20], str2[30];
+    char str1[20];
+    int n;
+    int * dynamicArray;
+    map <int, int> hash;
+
+    cin>> n;
+    dynamicArray = new int[n] ;
 
    printf("Enter name: ");
    scanf("%s", str1);
-
-   printf("Enter your website name: ");
-   scanf("%s", str2);
-
    printf("Entered Name: %s\n", str1);
-   printf("Entered Website:%s", str2);
    
     return 0;
 }
